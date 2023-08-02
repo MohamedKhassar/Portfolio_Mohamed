b7a4b32be37fcd01467bf14ff1dcbd9f6253c496
khassarMohamed <mohamedkhassar775.com>
2023-08-02 17:41:09 +0100

    commit 3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import "./header.css"
+import CTA from './CTA';
+import ME from "../../assets/36.png"
+import HeaderSocial from './HeaderSocial';
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Mohamed Khassar</h1>
+                <h5 className="text-light">Full-Stack Developer</h5>
+                <CTA />
+                <HeaderSocial/>
+                <div className="me">
+                    <img src={ME} alt="" />
+                </div>
+
+                <a href="#contact" className='scroll_down'>Scrool Down</a>
+            </div>
+        </header>
     );
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import "./nav.css"
+import {AiFillHome }  from "react-icons/ai";
+import {GiSkills}  from "react-icons/gi";
+import {BiSolidUser}  from "react-icons/bi";
+import {BsFillChatDotsFill}  from "react-icons/bs";
+import {MdMiscellaneousServices}  from "react-icons/md";
 const Nav = () => {
     return (
-        <div>
-             
-        </div>
+        <nav>
+            <a href="#"><AiFillHome /></a>
+            <a href="#about"><BiSolidUser /></a>
+            <a href="#experience"><GiSkills /></a>
+            <a href="#services"><MdMiscellaneousServices /></a>
+            <a href="#contact"><BsFillChatDotsFill /></a>
+        </nav>
     );
 }
 
