f43d36aa3cc2477b06766ef4a9370db7ea47f7d5
Mohamed Khassar <mohamedkhassar775@gmail.com>
2023-10-24 14:37:25 +0100

    commit

--- src/components/body/SlideImg.jsx
+++ src/components/body/SlideImg.jsx
@@ -1,12 +1,9 @@
 import img from "../../assets/36.png"
-import Fade from "react-reveal"
 
 const SlideImg = () => {
   return (
     <div className="flex justify-end -translate-x-[200px] translate-y-[280px]">
-    <Fade right>
       <img src={img} alt=""  className="w-[300px] dark:border-cyan-900 rounded-full border-blue-900 border-8" />
-    </Fade>
     </div>
   );
 }
