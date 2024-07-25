7246b75fe60ec9ad74d6f4a9095a5f62e213b485
Mohamed Khassar <mohamedkhassar775@gmail.com>
2024-07-25 01:05:53 +0100

    "Updated styles and layout for buttons and resume link on HomePage component"

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -12,9 +12,9 @@ const HomePage = () => {
                     <h1 className="capitalize lg:text-8xl text-5xl font-extrabold font-Poppins"><span className="text-[#6e06f2]">Full-</span>Stack<br /> developer</h1>
                     <p className="lg:w-[42rem] leading-9 lg:text-xl text-sm text-[#6F7589] font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta repellat culpa dolorem cum similique molestias quo magni asperiores placeat vel. Repellendus impedit nesciunt unde dolore deleniti in qui quia eligendi?
                     </p>
-                    <div className="space-x-8 flex lf:justify-center">
-                        <a download={"Mohamed_Khassar_CV(ENG).pdf"} href="/public/assets/files/Mohamed_Khassar_CV(ENG).pdf" className="outline-none shadow-2xl shadow-black/80 py-3 px-5 lg:text-xl bg-black text-white capitalize lg:font-semibold font-bold rounded-md flex gap-x-3 text-sm">my resume <ArrowDown className="animate-bounce" /> </a>
-                        <button className="text-sm outline-none shadow-2xl shadow-slate-500/80 py-3 px-5 lg:text-xl text-black bg-[#ebebeb] hover:bg-[#d8d8d8] duration-300 capitalize lg:font-semibold font-bold rounded-md">browse my projects</button>
+                    <div className="lg:space-x-8 space-x-5 flex lg:justify-start text-nowrap justify-center">
+                        <a download={"Mohamed_Khassar_CV(ENG).pdf"} href="/public/assets/files/Mohamed_Khassar_CV(ENG).pdf" className="outline-none shadow-2xl shadow-black/80 py-3 px-5 lg:text-xl bg-black text-white capitalize font-semibold rounded-md flex gap-x-3 text-xs items-center">my resume <ArrowDown className="animate-bounce lg:size-max size-5" /> </a>
+                        <button className="text-xs outline-none shadow-2xl shadow-slate-500/80 py-3 px-5 lg:text-xl text-black bg-[#ebebeb] hover:bg-[#d8d8d8] duration-300 capitalize font-semibold rounded-md">browse my projects</button>
                     </div>
                 </div>
                 <div className="flex w-full">
