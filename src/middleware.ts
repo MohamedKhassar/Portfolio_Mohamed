fc6d50ada27f9bad661df9804467b0c0ed5a6696
Mohamed Khassar <mohamedkhassar775@gmail.com>
2024-08-20 03:52:03 +0100

    "Refactor middleware function and simplify project title matching logic"

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,13 @@ import { NextRequest, NextResponse } from "next/server"
 import projects from "./data/Data"
 
 export function middleware(request: NextRequest) {
-    const name = request.nextUrl.pathname.replaceAll("/project/", "")
-    const project = projects.find(project => project.title.toLowerCase() == name.toLowerCase().replaceAll("%20", " "))
-    if (!project) {
-        return NextResponse.redirect(new URL('/not-found', request.nextUrl.origin))
-    } else {
-        return NextResponse.next()
+    const name = request.nextUrl.pathname
+    if (name.startsWith('/project/')) {
+        const project = projects.find(project => project.title.toLowerCase() == name.toLowerCase().replaceAll("/project/", "").replaceAll("%20", " "))
+        if (!project) {
+            return NextResponse.redirect(new URL('/not-found', request.nextUrl.origin))
+        } else {
+            return NextResponse.next()
+        }
     }
-}
-
-export const config = {
-    matcher: '/project((?!_next|static).*)'
 }
