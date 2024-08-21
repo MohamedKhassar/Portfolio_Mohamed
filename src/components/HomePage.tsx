38a2dc8803c36fa9fcf123359656f835a8f0e112
Mohamed Khassar <mohamedkhassar775@gmail.com>
2024-08-21 23:39:37 +0100

    add drop down to "my resume" button for french and english resume and update the description of projects and make it like drop down

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,29 @@
 "use client"
 import { Fade } from "react-awesome-reveal"
 import moh from "/public/assets/imgs/Mohamed.jpg"
-import { ArrowDown } from "lucide-react"
+import { ArrowDown, ChevronDown, ChevronUp } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { useEffect, useRef, useState } from "react"
+import { cn } from "@/lib/utils"
 
 const HomePage = () => {
+    const [isDropDown, setIsDropDown] = useState(false)
+    const ref = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                setIsDropDown(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref]);
+
     return (
         <div id="home" className="flex lg:flex-row flex-col-reverse gap-y-20 items-center lg:justify-between lg:my-40 my-20 lg:mx-20 mx-8 -z-10">
             <Fade cascade>
@@ -15,7 +33,16 @@ const HomePage = () => {
                     <p className="lg:w-[42rem] leading-9 lg:text-xl text-xs text-[#6F7589] font-medium">Hello! I'm Mohamed, a passionate Full-stack Developer skilled in JavaScript, TypeScript, Python, NodeJS, ReactJS, and ExpressJS, with some experience in Django. My love for technology drives me to create dynamic and responsive web applications.</p>
 
                     <div className="lg:space-x-8 space-x-5 flex lg:justify-start text-nowrap justify-center">
-                        <Link target="_blank" rel="noopener noreferrer" href="/assets/files/Mohamed_Khassar_CV(ENG).pdf" className="outline-none shadow-2xl shadow-[#6e06f2]/80 py-3 px-5 lg:text-xl bg-[#6e06f2] text-white capitalize font-bold rounded-md flex gap-x-3 text-sm items-center">my resume<ArrowDown className="animate-bounce lg:size-max size-5" /> </Link>
+                        <div ref={ref} className="relative">
+                            <button onClick={() => setIsDropDown(!isDropDown)} className="outline-none shadow-2xl shadow-[#6e06f2]/8 py-3 px-5 lg:text-xl bg-[#6e06f2] text-white capitalize font-bold rounded-md flex gap-x-3 text-sm items-center justify-center z-50">my resume <ChevronDown className={cn("duration-300", isDropDown && "rotate-180")} /></button>
+                            <div className={cn("space-y-4 absolute opacity-0 flex-col items-start justify-start left-0 w-full my-5 -z-30",
+                                isDropDown ? "animate-dropdown flex" : "hide-dropdown"
+                            )}>
+                                {Array("en français", "in english").map((item, i) => (
+                                    <Link target="_blank" rel="noopener noreferrer" href={`/assets/files/Mohamed_Khassar_CV(${item.includes("français") ? "FR" : "ENG"}).pdf`} className="flex items-center justify-between p-3 border rounded-md text-center w-full font-Poppins text-xl font-semibold capitalize gap-x-5 text-[#6e06f2] border-[#6e06f2] hover:text-white link hover:bg-[#6c06f2]/80 duration-300">{item}<ArrowDown className="lg:size-max size-5" /> </Link>
+                                ))}
+                            </div>
+                        </div>
                         <button className="text-sm outline-none shadow-2xl shadow-slate-500/80 py-3 px-5 lg:text-xl text-black bg-[#ebebeb] hover:bg-[#d8d8d8] duration-300 capitalize font-bold rounded-md" onClick={() => scrollTo(0, Number(document.getElementById("projects")?.offsetTop) - 200)}>browse projects </button>
                     </div>
                 </div>
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,62 +1,91 @@
 "use client"
 import Link from "next/link"
-import { ArrowRightCircle } from "lucide-react"
+import { ArrowRightCircle, ChevronDown } from "lucide-react"
 import { Fade } from "react-awesome-reveal"
 import Image from "next/image"
 import projects from "@/data/Data"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { cn } from "@/lib/utils"
 
 const ProjectsSection = () => {
     const refs = useRef<{ [key: string]: HTMLSpanElement | null }>({});
-    const handleHover = (index: any) => {
+    const iconRefs = useRef<(HTMLDivElement | null)[]>([]);
+    const [isDropDownId, setIsDropDownId] = useState<number | null>(null);
+
+    const handleHover = (index: string) => {
         const spanElement = refs.current[index];
         if (spanElement) {
             spanElement.style.opacity = "1";
         }
     };
 
-    const handleMouseLeave = (index: any) => {
+    const handleMouseLeave = (index: string) => {
         const spanElement = refs.current[index];
         if (spanElement) {
             spanElement.style.opacity = "0";
         }
     };
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (isDropDownId !== null && iconRefs.current[isDropDownId] && !iconRefs.current[isDropDownId]?.contains(event.target as Node)) {
+                setIsDropDownId(null);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isDropDownId]);
+
     return (
         <div id="projects" className="lg:mx-20 lg:my-44 my-20 space-y-10">
             <Fade direction="left">
                 <h1 className='capitalize font-bold font-Poppins text-5xl lg:text-8xl flex items-center text-nowrap mx-3'>projects<span className='lg:text-9xl text-6xl font-bold text-[#6e06f2]'>.</span></h1>
             </Fade>
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 place-items-center gap-10 lg:overflow-x-visible lg:mx-16">
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 place-items-start gap-10 lg:overflow-x-visible lg:mx-16">
                 <Fade>
-
                     {projects.map((item, i) =>
-                        <div className="rounded-3xl lg:w-fit mx-5 p-5 shadow-2xl lg:flex flex-col justify-between md:shadow-black/50  shadow-[#6e06f2]/70 md:hover:shadow-[#6e06f2]/70 duration-300 md:hover:scale-105 lg:h-[42rem] lg:space-y-0 space-y-10" key={item.id}>
+                        <div className="rounded-3xl lg:w-fit mx-5 p-5 shadow-2xl lg:flex flex-col justify-between gap-y-5 md:shadow-black/50 shadow-[#6e06f2]/70 md:hover:shadow-[#6e06f2]/70 duration-300 md:hover:scale-105 lg:h-fit lg:space-y-0 space-y-10" key={item.id}>
                             <Image className="rounded-md lg:h-64 object-cover w-full h-full object-center" src={item.image} width={1000} height={1000} alt={item.image} />
                             <div className="space-y-3">
-                                <Link className="flex items-center gap-x-5 w-fit" href={`/project/${item.title}`}>
-                                    <h2 className="uppercase font-extrabold font-Poppins lg:text-xl">{item.title}</h2>
-                                    <ArrowRightCircle className="border border-[#6e06f2] rounded-full text-[#6e06f2] hover:bg-[#6e06f2] duration-300 transform -rotate-45 hover:text-white lg:size-6 size-5" />
-                                </Link>
-                                <p className="lg:text-lg text-sm text-[#6F7589] w-full">{item.description}</p>
+                                <div className="flex justify-between">
+                                    <Link className="flex items-center gap-x-5 w-fit" href={`/project/${item.title}`}>
+                                        <h2 className="uppercase font-extrabold font-Poppins lg:text-xl">{item.title}</h2>
+                                        <ArrowRightCircle className="border border-[#6e06f2] rounded-full text-[#6e06f2] hover:bg-[#6e06f2] duration-300 transform -rotate-45 hover:text-white lg:size-6 size-5" />
+                                    </Link>
+                                    <div
+                                        className={cn("size-8 bg-[#6e06f2]/50 flex justify-center items-center rounded-full cursor-pointer",
+                                            isDropDownId === i && "rotate-180 duration-300")}
+                                        ref={(el) => { iconRefs.current[i] = el }}
+                                        onClick={() => setIsDropDownId(prev => prev !== i ? i : null)}
+                                    >
+                                        <ChevronDown />
+                                    </div>
+                                </div>
+                                <div className={cn("overflow-hidden", isDropDownId === i ? "animate-desc" : "h-7")}>
+                                    <p className="lg:text-lg text-sm text-[#6F7589]">{item.description}</p>
+                                </div>
                             </div>
-                            <div className="space-y-5">
+                            <div className="space-y-5 bg-white py-5">
                                 <h1 className="text-2xl font-Poppins font-bold capitalize">stack:</h1>
                                 <ul className="flex gap-5 items-center">
                                     {item.stack.map((s, index) => (
                                         <div
                                             key={index}
                                             className="relative flex flex-col justify-end items-center"
-                                            onMouseEnter={() => handleHover(Number(item.id + s!.id))}
-                                            onMouseLeave={() => handleMouseLeave(Number(item.id + s!.id))}
+                                            onMouseEnter={() => handleHover(`${item.id}${s!.id}`)}
+                                            onMouseLeave={() => handleMouseLeave(`${item.id}${s!.id}`)}
                                         >
                                             <li>{s?.icon()}</li>
                                             <span
                                                 ref={(el) => {
                                                     if (el) {
-                                                        refs.current[item.id + s!.id] = el;
+                                                        refs.current[`${item.id}${s!.id}`] = el;
                                                     }
                                                 }}
-                                                id={`title-${Number(item.id + s!.id)}`}
+                                                id={`title-${item.id}${s!.id}`}
                                                 className="capitalize absolute text-nowrap -bottom-12 rounded-md text-white font-Poppins p-2 opacity-0 duration-300 bg-[#6e06f2]"
                                             >
                                                 {s!.title}
@@ -66,12 +95,11 @@ const ProjectsSection = () => {
                                 </ul>
                             </div>
                         </div>
-                    ).reverse()
-                    }
+                    ).reverse()}
                 </Fade>
-            </div >
-        </div >
-    )
+            </div>
+        </div>
+    );
 }
 
-export default ProjectsSection
+export default ProjectsSection;
