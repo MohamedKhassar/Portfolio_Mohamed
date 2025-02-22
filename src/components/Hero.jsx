a36f058877e3c3b4727c2a3c5116a43e132adb27
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-02-23 00:38:10 +0100

    add fiverr button to see my services to buy it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { FiArrowDown, FiDownload } from "react-icons/fi";
-import {ButtonPrimary ,ButtonOutline} from "./Button"
+import { ButtonPrimary, ButtonOutline } from "./Button"
+import { SiFiverr } from "react-icons/si";
 const Hero = () => {
     return (
         <section className="pt-28 lg:pt-36" id="home">
@@ -28,7 +29,7 @@ const Hero = () => {
                     </h2>
                     <div className="flex items-center gap-3">
                         <ButtonPrimary label={"download cv"} icon={<FiDownload className="text-[18px]" />} href="/assets/file/Mohamed_Khassar_CV(ENG).pdf" target="_blank" classes={"capitalize group"} />
-
+                        <ButtonPrimary href="https://www.fiverr.com/s/YRx380q" classes={"capitalize !bg-green-900 !text-white hover:!bg-green-50 hover:!text-green-900 !duration-500"} target="_blank" icon={<SiFiverr className="md:size-14 size-12" />} />
                         <ButtonOutline href="#about" label={"scroll down"} classes={"capitalize"} icon={<FiArrowDown className="animate-bounce" />} />
                     </div>
                 </div>
