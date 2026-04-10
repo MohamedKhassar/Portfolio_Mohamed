c2f96672a3c500d1685a8b8f14324c500fecbcf9
Mohamed Khassar <mohamedkhassar775@gmail.com>
2026-04-11 00:36:46 +0100

    feat: add i18n support for Certificates and Skills components with English and German translations

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,13 +1,15 @@
+import { useTranslation } from 'react-i18next'
 import { certificates } from '../data'
 import AnimatedHeader from '../UI/AnimatedHeader'
 import CertificateCard from '../UI/CertificateCard'
 
 const Certificates = () => {
+    const { t } = useTranslation("certificate")
     return (
         <section id="certificates" className="section">
             <div className="container space-y-8">
                 <AnimatedHeader className={"headline-2 capitalize"}>
-                    My certificates
+                    {t("title")}
                 </AnimatedHeader>
                 <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(350px,_1fr))]">
                     {certificates.map(({ imgSrc, title, tags, certificateLink }, key) => (
--- src/i18n.js
+++ src/i18n.js
@@ -8,14 +8,20 @@ import enHero from "./locales/en/hero.json";
 import deHero from "./locales/de/hero.json";
 import enAbout from "./locales/en/about.json";
 import deAbout from "./locales/de/about.json";
+import enSkills from "./locales/en/skills.json";
+import deSkills from "./locales/de/skills.json";
+import enCertificate from "./locales/en/certificate.json";
+import deCertificate from "./locales/de/certificate.json";
+
+
 
 i18n
   .use(LanguageDetector) // 👈 detects navigator.language automatically
   .use(initReactI18next)
   .init({
     resources: {
-      en: { navbar: enNavbar, hero: enHero ,about: enAbout},
-      de: { navbar: deNavbar, hero: deHero ,about: deAbout},
+      en: { navbar: enNavbar, hero: enHero ,about: enAbout, skills: enSkills, certificate: enCertificate},
+      de: { navbar: deNavbar, hero: deHero ,about: deAbout, skills: deSkills, certificate: deCertificate},
     },
     fallbackLng: "en", // 👈 default when language not supported
     supportedLngs: ["en", "de"], // 👈 only these two, nothing else
